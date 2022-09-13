fc5b6cf80827b09dcd29587da83d0fe560408847
Ian Pedraza <ianpedrazacelon@gmail.com>
2022-09-13 12:52:02 -0500

    first commit

---
+++ app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.example.android.marsrealestate
+
+import android.content.Context
+import android.view.View
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.android.marsrealestate.network.MarsProperty
+import com.example.android.marsrealestate.overview.MarsApiStatus
+import com.example.android.marsrealestate.overview.PhotoGridAdapter
+
+@BindingAdapter("imgUrl")
+fun ImageView.bindImage(imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = it.toUri().buildUpon().scheme("http").build()
+        Glide.with(this.context)
+            .load(imgUri)
+            .placeholder(R.drawable.loading_animation)
+            .error(R.drawable.ic_connection_error)
+            .into(this)
+    }
+}
+
+@BindingAdapter("listData")
+fun RecyclerView.bind(data: List<MarsProperty>?) {
+    val adapter = this.adapter as PhotoGridAdapter
+    adapter.submitList(data)
+}
+
+fun Context.loadImage(imageView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = it.toUri().buildUpon().scheme("http").build()
+        Glide.with(this)
+            .load(imgUri)
+            .placeholder(R.drawable.loading_animation)
+            .error(R.drawable.ic_connection_error)
+            .into(imageView)
+    }
+}
+
+@BindingAdapter("marsApiStatus")
+fun ImageView.bindStatus(status: MarsApiStatus?) {
+    status?.let {
+        when (status) {
+            MarsApiStatus.LOADING -> {
+                visibility = View.VISIBLE
+                setImageResource(R.drawable.loading_animation)
+            }
+            MarsApiStatus.ERROR -> {
+                visibility = View.VISIBLE
+                setImageResource(R.drawable.ic_connection_error)
+            }
+            MarsApiStatus.DONE -> {
+                visibility = View.GONE
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/marsrealestate/overview/PhotoGridAdapter.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.example.android.marsrealestate.overview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.marsrealestate.databinding.GridViewItemBinding
+import com.example.android.marsrealestate.network.MarsProperty
+
+class PhotoGridAdapter(
+    private val onClickListener: OnClickListener,
+) : ListAdapter<MarsProperty, PhotoGridAdapter.MarsPropertyViewHolder>(DiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarsPropertyViewHolder {
+        return MarsPropertyViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: MarsPropertyViewHolder, position: Int) {
+        val marsProperty = getItem(position)
+        holder.bind(marsProperty, onClickListener)
+    }
+
+    class MarsPropertyViewHolder(
+        private val binding: GridViewItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        companion object {
+            fun from(parent: ViewGroup): MarsPropertyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = GridViewItemBinding.inflate(layoutInflater, parent, false)
+                return MarsPropertyViewHolder(binding)
+            }
+        }
+
+        fun bind(marsProperty: MarsProperty, onClickListener: OnClickListener) {
+            binding.property = marsProperty
+            binding.root.setOnClickListener {
+                onClickListener.onClick(marsProperty)
+            }
+            binding.executePendingBindings()
+        }
+
+    }
+
+    companion object DiffCallback : DiffUtil.ItemCallback<MarsProperty>() {
+        override fun areItemsTheSame(oldItem: MarsProperty, newItem: MarsProperty): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: MarsProperty, newItem: MarsProperty): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    class OnClickListener(val clickListener: (marsProperty: MarsProperty) -> Unit) {
+        fun onClick(marsProperty: MarsProperty) = clickListener(marsProperty)
+    }
+}
